1abf51fde0179717bb0afc24ab5d962b3e86a2e1
Veszter Márton <vmarci21@msn.com>
2016-01-03 15:47:57 +0100

    Update using_test.js

--- using_test.js
+++ using_test.js
@@ -4,7 +4,7 @@ if (document.getElementsByClassName('kepgaleria').length > 0) {
   var kepek = document.getElementsByClassName("kepgaleria_kep");
   for (var i = 0; i < kepek.length; i++) {
     kepek[i].addEventListener("click", function() {
-      mutatkep(this.dataset.bigsrc,this.title,'kepgaleria_kep');
+      showimage(this.dataset.bigsrc,this.title,'kepgaleria_kep');
 
     });
   }
