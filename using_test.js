8b5e74ad566fe008462e00613320a3e9665c838f
Veszter Márton <vmarci21@msn.com>
2016-01-03 15:51:49 +0100

    Update using_test.js

--- using_test.js
+++ using_test.js
@@ -1,9 +1,9 @@
 window.addEventListener("load",function(){
 if (document.getElementsByClassName('imagegallery').length > 0) {
 
-  var kepek = document.getElementsByClassName("imagegallery");
-  for (var i = 0; i < kepek.length; i++) {
-    kepek[i].addEventListener("click", function() {
+  var images = document.getElementsByClassName("imagegallery");
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", function() {
       showimage(this.dataset.bigsrc,this.title,'imagegallery');
 
     });
