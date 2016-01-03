2219a8d5e005e1bf1b53aae78a6519cb158f2a43
Veszter Márton <vmarci21@msn.com>
2016-01-03 17:58:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,203 +1,227 @@
-elsokep = true;
-keptarti = 0;
-mennyiea = 1;
-gallerye = '';
-vantov = false;
-vanvissz = false;
-kepnagyitonyitva = false;
-function showimage(url,text,gallery,ie) {
-kepnagyitonyitva = true;
-var nextkepurl = '';
-var mennyie = 1;
-var mennyie2 = 2;
+/*ImagepopUp JS 
+Version: 1.0
+http://intomedia.hu
+https://github.com/vmarci21/PhotoPopUp
+*/
+var imagepopup = {
+firstimage: true,
+imageherei: 0,
+wherediv: 1,
+gallerye: '',
+hasnext: false,
+hasprev: false,
+opened: false,
+showimage: function(url,text,gallery,ie) {
+this.opened = true;
+var nextimageurl = '';
+var wherediv1 = 1;
+var wherediv2 = 2;
 if(ie==undefined || ie=='' || ie==null){ie=0;}
-if(elsokep){
-window.addEventListener("resize",function(){kepmeretezes();});
-mobiledrag();
-keyboard_view();
-elsokep = false;
+if(this.firstimage){
+window.addEventListener("resize",function(){imagepopup.resizeimage();});
+imagepopup.mobiledrag();
+imagepopup.keyboard_view();
+this.firstimage = false;
 }
 if(gallery!=undefined){
-gallerye = gallery;
-  var kepek = document.getElementsByClassName(gallery);
-  for (var i = ie; i < kepek.length; i++) {
-    if(kepek[i].dataset.bigsrc==url || kepek[i].src==url){
-    keptarti = i;
-    if(keptarti%2==0){mennyie = 1;mennyie2 = 2;}else{mennyie = 2;mennyie2 = 1;}
+this.gallerye = gallery;
+  var imagelist = document.getElementsByClassName(gallery);
+  for (var i = ie; i < imagelist.length; i++) {
+    if(imagelist[i].dataset.bigsrc==url || imagelist[i].src==url){
+    this.imageherei = i;
+    if(this.imageherei%2==0){wherediv1 = 1;wherediv2 = 2;}else{wherediv1 = 2;wherediv2 = 1;}
     if(i>0){
-    vanvissz = true;
-     document.getElementById('kepnagyito_prev').className = 'active';
+    this.hasprev = true;
+     document.getElementById('imagepopup_prev').className = 'active';
     }else{
-    vanvissz = false;
-    document.getElementById('kepnagyito_prev').className = '';
+    this.hasprev = false;
+    document.getElementById('imagepopup_prev').className = '';
     }
-    if(kepek.length>i+1){
-     document.getElementById('kepnagyito_next').className = 'active';
-    if(kepek[i].dataset.bigsrc!=undefined){
-     nextkepurl = kepek[i+1].dataset.bigsrc;
+    if(imagelist.length>i+1){
+     document.getElementById('imagepopup_next').className = 'active';
+    if(imagelist[i].dataset.bigsrc!=undefined){
+     nextimageurl = imagelist[i+1].dataset.bigsrc;
      }else{
-     nextkepurl = kepek[i+1].src;
+     nextimageurl = imagelist[i+1].src;
      }
-     vantov = true;
+     this.hasnext = true;
     }else{
-    vantov = false;
-			document.getElementById('kepnagyito_next').className = '';
+    this.hasnext = false;
+			document.getElementById('imagepopup_next').className = '';
     }
-    i = kepek.length;
+    i = imagelist.length;
     }
   }
 }
 
-document.getElementById('kepnagyito').style.display = 'block';
-document.getElementById('kepnagyito_load').className = 'active';
-setTimeout(function(){ document.getElementById('kepnagyito').className = 'active'; }, 30);
+document.getElementById('imagepopup').style.display = 'block';
+document.getElementById('imagepopup_load').className = 'active';
+setTimeout(function(){ document.getElementById('imagepopup').className = 'active'; }, 30);
 
 var newImg = new Image;
+newImg.onerror = function() {
+document.getElementById('imagepopup_load').className = '';
+imagepopup.showtext('Loading errors','Failed to load the picture :(');
+};
+
 newImg.onload = function() {
-		mennyiea = mennyie;
-    document.getElementById('kepnagyito_load').className = '';
-    document.getElementById('kepnagyito_kep'+mennyie2).className = '';
-    document.getElementById('kepnagyito_kep'+mennyie).className = 'active';
-    document.getElementById('kepnagyito_kep'+mennyie).innerHTML = '<img id="nagy_kep'+mennyie+'" src="'+url+'">';
-    kepmeretezes();
-    document.getElementById('kepnagyito_felso').innerHTML = '<a href="'+url+'">Link</a> <a href="javascript://" onclick="elrejtkep();">x</a>';
-    document.getElementById('kepnagyito_szoveg').innerHTML = text;
-    if(nextkepurl!=''){
+		imagepopup.wherediv = wherediv1;
+    document.getElementById('imagepopup_load').className = '';
+    document.getElementById('imagepopup_image'+wherediv2).className = '';
+    document.getElementById('imagepopup_image'+wherediv1).className = 'active';
+    document.getElementById('imagepopup_image'+wherediv1).innerHTML = '<img id="nagy_kep'+wherediv1+'" src="'+url+'">';
+    imagepopup.resizeimage();
+    document.getElementById('imagepopup_panel').innerHTML = '<a href="'+url+'" target="_blank">Link</a> <a href="javascript://" onclick="imagepopup.hideimage();">x</a>';
+    document.getElementById('imagepopup_text').innerHTML = text;
+    if(nextimageurl!=''){
      var newImg2 = new Image;
-     newImg2.src = nextkepurl;
+     newImg2.src = nextimageurl;
     }
 }
 newImg.src = url;
-}
+},
 
-function next(){
- var kepek = document.getElementsByClassName(gallerye);
-if(kepek[keptarti+1].dataset.bigsrc!=undefined){
-     var url = kepek[keptarti+1].dataset.bigsrc;
+showtext: function(title,text){
+document.getElementById('imagepopup').style.display = 'block';
+document.getElementById('imagepopup_image1').innerHTML = '<div id="window"><h3 class="title">'+title+'</h3><p>'+text+'</p></div>';
+document.getElementById('imagepopup_panel').innerHTML = '<a href="javascript://" onclick="imagepopup.hideimage();">x</a>';
+setTimeout(function(){ document.getElementById('imagepopup').className = 'active'; 
+document.getElementById('imagepopup').className = 'active';
+document.getElementById('imagepopup_image1').className = 'active';
+document.getElementById('imagepopup_image2').className = '';
+}, 30);
+},
+
+
+
+next: function(){
+ var imagelist = document.getElementsByClassName(this.gallerye);
+if(imagelist[this.imageherei+1].dataset.bigsrc!=undefined){
+     var url = imagelist[this.imageherei+1].dataset.bigsrc;
      }else{
-     var url = kepek[keptarti+1].src;
+     var url = imagelist[this.imageherei+1].src;
      }
- showimage(url,kepek[keptarti+1].title,gallerye,keptarti);
- }
- function prev(){
- var kepek = document.getElementsByClassName(gallerye);
-if(kepek[keptarti-1].dataset.bigsrc!=undefined){
-     var url = kepek[keptarti-1].dataset.bigsrc;
+ this.showimage(url,imagelist[this.imageherei+1].title,this.gallerye,this.imageherei);
+ },
+ prev: function(){
+ var imagelist = document.getElementsByClassName(this.gallerye);
+if(imagelist[this.imageherei-1].dataset.bigsrc!=undefined){
+     var url = imagelist[this.imageherei-1].dataset.bigsrc;
      }else{
-     var url = kepek[keptarti-1].src;
+     var url = imagelist[this.imageherei-1].src;
      }
- showimage(url,kepek[keptarti-1].title,gallerye,keptarti-1);
- }
+ this.showimage(url,imagelist[this.imageherei-1].title,this.gallerye,this.imageherei-1);
+ },
 
-function elrejtkep2(t){
-if(t.target.id=='kepnagyito' || t.target.id=='kepnagyito_kep1' || t.target.id=='kepnagyito_kep2'){
-elrejtkep();
+hideimage2: function(t){
+if(t.target.id=='imagepopup' || t.target.id=='imagepopup_image1' || t.target.id=='imagepopup_image2'){
+this.hideimage();
 }
-}
+},
 
-function elrejtkep(){
-kepnagyitonyitva = false;
- document.getElementById('kepnagyito').className = '';
-setTimeout(function(){ document.getElementById('kepnagyito').style.display = 'none';
-document.getElementById('kepnagyito_szoveg').innerHTML = '';
-document.getElementById('kepnagyito_prev').className = '';
-document.getElementById('kepnagyito_next').className = '';
+hideimage: function(){
+this.opened = false;
+ document.getElementById('imagepopup').className = '';
+setTimeout(function(){ document.getElementById('imagepopup').style.display = 'none';
+document.getElementById('imagepopup_text').innerHTML = '';
+document.getElementById('imagepopup_prev').className = '';
+document.getElementById('imagepopup_next').className = '';
 }, 300);
 
-}
+},
 
-function kepmeretezes() {
-  if (document.getElementById('nagy_kep'+mennyiea)) {
-    var meret1 = document.getElementById('kepnagyito').offsetWidth;
-    var meret2 = document.getElementById('kepnagyito').offsetHeight;
+resizeimage: function() {
+  if (document.getElementById('nagy_kep'+this.wherediv)) {
+    var meret1 = document.getElementById('imagepopup').offsetWidth;
+    var meret2 = document.getElementById('imagepopup').offsetHeight;
     meret2 = meret2-40;
-    document.getElementById('nagy_kep'+mennyiea).style.maxWidth = meret1 + 'px';
-    document.getElementById('nagy_kep'+mennyiea).style.maxHeight = meret2 + 'px';
-    var meret3 = document.getElementById('nagy_kep'+mennyiea).offsetWidth;
+    document.getElementById('nagy_kep'+this.wherediv).style.maxWidth = meret1 + 'px';
+    document.getElementById('nagy_kep'+this.wherediv).style.maxHeight = meret2 + 'px';
+    var meret3 = document.getElementById('nagy_kep'+this.wherediv).offsetWidth;
     meret3 = meret3-20;
-    var meret5 = document.getElementById('nagy_kep'+mennyiea).offsetHeight;
+    var meret5 = document.getElementById('nagy_kep'+this.wherediv).offsetHeight;
     var meret4 = (meret2-meret5)/2
-    document.getElementById('nagy_kep'+mennyiea).style.marginTop = meret4+'px';
+    document.getElementById('nagy_kep'+this.wherediv).style.marginTop = meret4+'px';
     if(meret3>600){
-    document.getElementById('kepnagyito_szoveg').style.width = meret3 + 'px';
-    document.getElementById('kepnagyito_szoveg').className = 'kepben';
+    document.getElementById('imagepopup_text').style.width = meret3 + 'px';
+    document.getElementById('imagepopup_text').className = 'kepben';
     var meret6 = meret5+meret4+5;
-    document.getElementById('kepnagyito_szoveg').style.top = meret6+'px';
+    document.getElementById('imagepopup_text').style.top = meret6+'px';
     }else{
-    document.getElementById('kepnagyito_szoveg').style.width =  '';
-    document.getElementById('kepnagyito_szoveg').style.top = '';
-    document.getElementById('kepnagyito_szoveg').className = '';
+    document.getElementById('imagepopup_text').style.width =  '';
+    document.getElementById('imagepopup_text').style.top = '';
+    document.getElementById('imagepopup_text').className = '';
     }
   }
-}
-function keyboard_view(){
+},
+keyboard_view: function(){
 window.addEventListener('keyup', function(event) {
-if(kepnagyitonyitva){
+if(imagepopup.opened){
        var keyCode = ('which' in event) ? event.which : event.keyCode;
-        if(keyCode==37 && vanvissz){
-   prev();
-   }else if(keyCode==39 && vantov){
-   next();
+        if(keyCode==37 && imagepopup.hasprev){
+   imagepopup.prev();
+   }else if(keyCode==39 && imagepopup.hasnext){
+   imagepopup.next();
    }else if(keyCode==27){
-   elrejtkep();
+   imagepopup.hideimage();
    }
    }
 });
-}
+},
 
-function mobiledrag(){
+mobiledrag: function(){
 startx = 0;
 starty = 0;
 ennyit = 0;
 starttop = 0;
 ennyit2 = 0;
-document.getElementById('kepnagyito').addEventListener('touchstart', function(event) {
+document.getElementById('imagepopup').addEventListener('touchstart', function(event) {
    var touch = event.targetTouches[0];
     startx = touch.pageX;
     starty= touch.pageY;
-    starttop = document.getElementById('nagy_kep'+mennyiea).style.marginTop;
+    starttop = document.getElementById('nagy_kep'+imagepopup.wherediv).style.marginTop;
     starttop = starttop.replace('px','');
   }, false);
-  document.getElementById('kepnagyito').addEventListener('touchmove', function(event) {
+  document.getElementById('imagepopup').addEventListener('touchmove', function(event) {
    var touch = event.targetTouches[0];
    ennyit = touch.pageX-startx;
    ennyit2 = starty-touch.pageY;
    if(Math.abs(ennyit)>Math.abs(ennyit2)){
-    document.getElementById('nagy_kep'+mennyiea).style.marginLeft = ennyit+ 'px';
+    document.getElementById('nagy_kep'+imagepopup.wherediv).style.marginLeft = ennyit+ 'px';
    }else{
      var ennyit3 = starttop-ennyit2;
-     document.getElementById('nagy_kep'+mennyiea).style.marginTop = ennyit3+ 'px';
+     document.getElementById('nagy_kep'+imagepopup.wherediv).style.marginTop = ennyit3+ 'px';
     }
      event.preventDefault();
   }, false);
-  document.getElementById('kepnagyito').addEventListener('touchend', function(event) {
+  document.getElementById('imagepopup').addEventListener('touchend', function(event) {
    var hova = '0px';
    if(Math.abs(ennyit)>Math.abs(ennyit2)){
-   if(ennyit>200 && vanvissz){
+   if(ennyit>200 && imagepopup.hasprev){
    var hova = '100%';
-   }else if(ennyit<-200 && vantov){
+   }else if(ennyit<-200 && imagepopup.hasnext){
    var hova = '-200%';
    }
-    document.getElementById('nagy_kep'+mennyiea).className = 'touch';
-    document.getElementById('nagy_kep'+mennyiea).style.marginLeft = hova;
-   if(ennyit>200 && vanvissz){
-   prev();
-   }else if(ennyit<-200 && vantov){
-   next();
+    document.getElementById('nagy_kep'+imagepopup.wherediv).className = 'touch';
+    document.getElementById('nagy_kep'+imagepopup.wherediv).style.marginLeft = hova;
+   if(ennyit>200 && imagepopup.hasprev){
+   imagepopup.prev();
+   }else if(ennyit<-200 && imagepopup.hasnext){
+   imagepopup.next();
    }
    }else{
    if(ennyit2>100 || ennyit2<-100){
-    elrejtkep();
+    imagepopup.hideimage();
    }else{
    hova = starttop+'px';
    }
-   document.getElementById('nagy_kep'+mennyiea).className = 'touch';
-   document.getElementById('nagy_kep'+mennyiea).style.marginTop = hova;
+   document.getElementById('nagy_kep'+imagepopup.wherediv).className = 'touch';
+   document.getElementById('nagy_kep'+imagepopup.wherediv).style.marginTop = hova;
       }
   }, false);
-}
+},
 
-function kepreszmutat(idname) {
+hideview: function(idname) {
 	var ele = document.getElementById(idname);
 	if(ele.style.opacity == "1") {
     	ele.style.opacity = "0";
@@ -205,3 +229,4 @@ function kepreszmutat(idname) {
 		ele.style.opacity = "1";
 	}
 }
+};
