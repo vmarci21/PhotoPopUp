56fff81582dec055e9d16eb61ed6c4be6dc94066
Veszter Márton <vmarci21@msn.com>
2016-01-03 15:51:32 +0100

    Update using_test.js

--- using_test.js
+++ using_test.js
@@ -1,10 +1,10 @@
 window.addEventListener("load",function(){
-if (document.getElementsByClassName('kepgaleria').length > 0) {
+if (document.getElementsByClassName('imagegallery').length > 0) {
 
-  var kepek = document.getElementsByClassName("kepgaleria_kep");
+  var kepek = document.getElementsByClassName("imagegallery");
   for (var i = 0; i < kepek.length; i++) {
     kepek[i].addEventListener("click", function() {
-      showimage(this.dataset.bigsrc,this.title,'kepgaleria_kep');
+      showimage(this.dataset.bigsrc,this.title,'imagegallery');
 
     });
   }
