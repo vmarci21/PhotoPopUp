505678449fac1c876405b24888370dcf95f5bdb1
Veszter Márton <vmarci21@msn.com>
2016-01-03 18:39:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,6 @@ Version: 1.0
 http://intomedia.hu
 https://github.com/vmarci21/PhotoPopUp
 */
-window.addEventListener("load", function() {
-  if (document.getElementsByClassName('imagegallery').length > 0) {
-
-    var images = document.getElementsByClassName("imagegallery");
-    for (var i = 0; i < images.length; i++) {
-      images[i].addEventListener("click", function() {
-        imagepopup.showimage(this.dataset.bigsrc, this.title, 'imagegallery');
-
-      });
-    }
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-/*ImagepopUp JS*/
 var imagepopup = {
 firstimage: true,
 imageherei: 0,
