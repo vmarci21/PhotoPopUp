240e3a522742fa1af43172b5a1dfb111fb389bac
Veszter Márton <vmarci21@msn.com>
2016-05-13 16:08:46 +0200

    Update using_test.js

--- using_test.js
+++ using_test.js
@@ -1,12 +1,3 @@
 window.addEventListener("load", function() {
-  if (document.getElementsByClassName('imagegallery').length > 0) {
-
-    var images = document.getElementsByClassName("imagegallery");
-    for (var i = 0; i < images.length; i++) {
-      images[i].addEventListener("click", function() {
-        imagepopup.showimage(this.dataset.bigsrc, this.title, 'imagegallery');
-
-      });
-    }
-  }
+imagepopup.newgallery('imagegallery');
 });
